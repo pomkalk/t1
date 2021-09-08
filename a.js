2e5355aa2c2e2f86ffb4cd4e433cb13d89f6e673
Roman Cherepanov <r.cherepanov@transtelematica.ru>
2021-09-08 14:08:11 +0300

    s1

--- a.js
+++ a.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const { spawn, exec } = require('child_process')
+const iconv = require('iconv-lite')
+const unzipper = require('unzipper')
 
 const getCRC = async (file, b) => {
     return new Promise((resolve) => {
@@ -44,7 +46,7 @@ const getCRC = async (file, b) => {
 
 // ^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\s\.*?(\w)\.*?\s+?(\d+?)\s+?(\d+?)\s+?(.+?)$
 
-class Archive7z {
+class Archive7zo {
     static async check7z () {
         return new Promise((resolve, reject) => {
             exec('7z', (err, res) => {
@@ -59,7 +61,38 @@ class Archive7z {
 
     static async list (file) {
         return new Promise((resolve, reject) => {
-            let a = exec(`7z l "${file}" | iconv -f cp866 -t utf-8`, (err, res) => {
+            let a = exec(`7z l -sccDOS "${file}" | iconv -f cp866 -t utf-8`, (err, res) => {
+                console.log(res)
+                if (err) return reject(err)
+                let match_all = res.match(/^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\s\.*?([A-Z\.])\.*?\s+?(\d+?)\s+?(\d+?)\s+?(.+?)$/mg)
+                if (match_all) {
+                    let files = match_all.map(line => {
+                        let match = line.match(/^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\s\.*?([A-Z\.])\.*?\s+?(\d+?)\s+?(\d+?)\s+?(.+?)$/m)
+                        if (match) {
+                            return {
+                                date: match[1],
+                                time: match[2],
+                                is_dir: match[3].indexOf('D') >= 0,
+                                size: match[4],
+                                name: path.basename(match[6].trim()).slice(0, -path.extname(match[6]).length),
+                                path: match[6].trim(),
+                                ext: path.extname(match[6]).toLowerCase()
+                            }
+                        }
+                        return null;
+                    })
+                    resolve(files)
+                } else {
+                    resolve (null)
+                }
+            })
+        })
+    }
+
+    static async list2 (file) {
+        return new Promise((resolve, reject) => {
+            let a = exec(`7z l -sccDOS "${file}"`, (err, res) => {
+                console.log(res)
                 if (err) return reject(err)
                 let match_all = res.match(/^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\s\.*?([A-Z\.])\.*?\s+?(\d+?)\s+?(\d+?)\s+?(.+?)$/mg)
                 if (match_all) {
@@ -87,13 +120,8 @@ class Archive7z {
     }
 
     static async getBuffer (file, name) { 
-        let cmd = `7z e -so "${file}" "${name}"`;
-        if (process.platform === 'win32') {
-            cmd = `echo "${name}" | iconv -f utf-8 -t cp866 | xargs 7z e -so "${file}"`
-        }
-
         return new Promise((resolve, reject) => {
-            exec(`echo "${name}" | iconv -f utf-8 -t cp866`, { encoding: 'buffer' }, (err, res) => {
+            exec(`7z e -sccUTF-8 -so "${file}" "${name}"`, { encoding: 'buffer', maxBuffer: 1024*1024*1024 }, (err, res) => {
                 if (err) return reject(err)
                 resolve(res)
             })
@@ -110,7 +138,6 @@ class Archive7z {
                 data += chunk
             })
             proc.on('close', () => {
-                console.log(data)
                 const match = data.match(/^([A-Z0-9]{16})\s/m)
                 if (match) return resolve(match[0])
                 resolve(null)
@@ -140,24 +167,82 @@ class Archive7z {
             })
         })
     }
+
+    static async asd (file, name, la) { 
+        return new Promise((resolve, reject) => {
+            exec(`echo "${name}" | xargs 7z e -sccWIN -so "${file}"`, { encoding: 'utf-8', maxBuffer: 1024*1024*1024 }, (err, res) => {
+                if (err) return reject(err)
+                resolve(res)
+            })
+        })
+    }
+    static async lll () { 
+        return new Promise((resolve, reject) => {
+ 
+            exec(`iconv -l`, {encoding: 'utf-8'},  (err, res) => {
+                if (err) return reject(err)
+                resolve(res.toString())
+            })
+        })
+    }
+
+}
+
+class Archive7z {
+
+    static async check7z () {
+        return new Promise((resolve, reject) => {
+            exec('7z', (err, res) => {
+                if (err) {
+                    return resolve(false)
+                }
+                let match = res.match(/7-Zip\s(.+?)\s/m)
+                resolve(match ? true : false)
+            })
+        })
+    }
+
+    constructor (file_name) {
+        this.file_name = path.resolve(file_name)
+        this.extract_path = this.file_name.slice(0, -path.extname(this.file_name).length)
+        this.is_extracted = false
+    }
+
+    async extract () {
+        return new Promise((resolve, reject) => {
+            exec(`7z x -sccDOS -y -o"${this.extract_path}" "${this.file_name}"`, (err, res) => {
+                if (err) return reject(err)
+                this.is_extracted = true
+                resolve( res )
+            })
+        })
+    }
+
+    _getFiles (dir) {
+        let files = fs.readdirSync(dir);
+        let list = []
+        for (let file of files) {
+            let file_name = path.resolve(dir, file)
+            if (fs.statSync(file_name).isDirectory()) {
+                list.push(...this._getFiles(file_name))
+            } else {
+                list.push(file_name)
+            }
+        }
+        return list
+    }
+
+    getFiles () {
+        console.log('sss')
+        return this._getFiles(this.extract_path)
+    }
 }
 
 const main = async () => {
     let ar = './597_mnt2.zip'
     let j = './1.json'
     
-    console.log (await Archive7z.check7z())
-
-    let l = await Archive7z.list(ar)
-    //console.log(l)
-    console.log(process.platform)
-    let f = l.find(x => x.ext === '.mp3')
-    console.log(f)
-    let b = await Archive7z.getBuffer(ar, f.path)
-    console.log(b.toString())
-    // let crc = await Archive7z.crc64(b)
-    // console.log(crc)
-    // let s = await Archive7z.getAudioLength('./1.mp3')
-    // console.log(s)
+    let l = await Archive7zo.list(ar)
+   console.log(l)
 }
 main()
